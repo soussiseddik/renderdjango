bafe275c0e51078c345dbbb5d9b702e6d1d242eb
soussiseddik <soussiseddik88@gmail.com>
2023-12-27 00:48:11 +0100

    test

--- siteName/settings.py
+++ siteName/settings.py
@@ -76,16 +76,9 @@ WSGI_APPLICATION = 'siteName.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-'''
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
-    }
 }
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
