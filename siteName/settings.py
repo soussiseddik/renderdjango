47e7ad02d0384cce89257491f658841ae95886bb
soussiseddik <soussiseddik88@gmail.com>
2024-01-02 19:56:14 +0100

    test

--- siteName/settings.py
+++ siteName/settings.py
@@ -116,9 +116,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    
-    #BASE_DIR / "static",
-    os.path.join(BASE_DIR, 'static/')
+    BASE_DIR / "static",
+    "static/",
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'assest')
 
