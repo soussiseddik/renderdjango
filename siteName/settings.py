ebe2e0fe20f36ed40b71ffa291ae004a5377e092
soussiseddik <soussiseddik88@gmail.com>
2024-01-02 02:34:43 +0100

    test

--- siteName/settings.py
+++ siteName/settings.py
@@ -140,10 +140,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-u56_o2!4!9lfj#bsg_j8y*!yw%fp5-u*jrhkv23r$lcl!*mgt='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-#ALLOWED_HOSTS = []
-ALLOWED_HOSTS = str(os.environ.get('ALLOWED_HOSTS')).split(' ')
+ALLOWED_HOSTS = []
+#ALLOWED_HOSTS = str(os.environ.get('ALLOWED_HOSTS')).split(' ')
 
 
 # Application definition
