9614c5c9fae25a7ad919fac938666d04ec2fd5e6
soussiseddik <soussiseddik88@gmail.com>
2024-01-02 21:35:53 +0100

    test

--- siteName/settings.py
+++ siteName/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 import os
 
 import dj_database_url
-
+'''
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -250,4 +250,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-'''
